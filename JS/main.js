380f1b00d79c559eec74c04baf36dc947a192960
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-10 15:39:28 +0200

    Stampo messaggio in h1, utilizzando i data.

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+// CONSEGNA: 
+//stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+const { createApp } = Vue
+createApp({
+    data() {
+        return {
+            message: "Hello Veu World",
+
+        }
+    }
+}).mount("#app")
+
